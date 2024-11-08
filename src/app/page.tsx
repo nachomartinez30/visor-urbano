6c6d4ee4e90f08f3d61eab49a9f88e80b71339f9
nachomartinez3010 <nachomartinez3010@gmail.com>
2024-11-08 14:49:16 -0600

    quienes somos section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Badge } from "@/components/Badge";
+import { QuienesSomos } from "@/components/QuienesSomos";
 
 export default function Home() {
   return (
     <>
       <Badge />
+      <QuienesSomos />
     </>
   );
 }
---
+++ src/components/QuienesSomos.tsx
@@ -0,0 +1,12 @@
+export const QuienesSomos = () => {
+  return (
+    <section id="quienes_somos" className="bg-orange-300 h-svh">
+      <h3 className="text-blue-500">¿Qué es Visor Urbano?</h3>
+      <iframe
+        src="https://www.youtube.com/embed/CR6C86CtG1w?si=a6TOi1lH-o_4AAob"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </section>
+  );
+};

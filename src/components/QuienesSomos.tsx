e00c21d1d3c4e8ae7cb20c53be783ce8da804769
[Jorgejalvarez] <[jorge.alvarez@conafor.gob.mx]>
2024-11-11 14:01:36 -0600

    Seccion Visor Urbano Video

--- src/components/QuienesSomos.tsx
+++ src/components/QuienesSomos.tsx
@@ -3,21 +3,19 @@ import video from "../assets/images/youtubePortada.png";
 export const QuienesSomos = () => {
   return (
     <section id="quienes_somos" className="lg:h-screen sm:h-1/4 somosBackground">
-      <h3 className="somos__Titulo text-center">¿Qué es Visor Urbano?</h3>
-      <iframe
-        src="https://www.youtube.com/embed/CR6C86CtG1w?si=a6TOi1lH-o_4AAob"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe>
-      <video 
-        controls
-        preload="true"
-        poster='../assets/images/youtubePortada.png'
-        autoPlay
-        web-share='true'
-        picture-in-picture='true'>
-        <source src="https://www.youtube.com/watch?v=CR6C86CtG1w" type='video/mp4'/>
-      </video>
+      <h3 className="somos__Titulo text-center mb-8">¿Qué es Visor Urbano?</h3>
+      <div className="flex justify-center">
+        <video 
+          controls
+          preload="true"
+          poster={video.src}
+          autoPlay
+          web-share='true'
+          picture-in-picture='true'
+          className='somos__Video'>
+          <source src="https://www.youtube.com/watch?v=CR6C86CtG1w" type='video/mp4'/>
+        </video>
+      </div>
     </section>
   );
 };

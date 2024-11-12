e44e07494fe63405c4cfd7c03f2737ce6497ccba
[Jorgejalvarez] <[jorge.alvarez@conafor.gob.mx]>
2024-11-12 09:36:17 -0600

    hotFix Video

--- src/components/QuienesSomos.tsx
+++ src/components/QuienesSomos.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { YouTubeEmbed } from "@next/third-parties/google";
+import idioma from "../assets/images/youtubePortada.png";
 
 export const QuienesSomos = () => {
   return (
@@ -10,7 +11,12 @@ export const QuienesSomos = () => {
       <h2 className="somos__Titulo text-center mb-8">¿Qué es Visor Urbano?</h2>
       <div className="flex justify-center">
         <YouTubeEmbed
-          class='somos__Video'
+          /* class='somos__Video' */
+          style={`width: 70%;
+                  min-width: 1200px!important;
+                  border-radius: 45px;
+                  background-image: url({${idioma}})!important;`
+                }
           videoid="CR6C86CtG1w"
         />
       </div>

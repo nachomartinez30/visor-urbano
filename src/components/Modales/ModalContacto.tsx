e0f4e85668a488838cb07b47102debe5bfcfce16
nachomartinez3010 <nachomartinez3010@gmail.com>
2024-11-12 19:54:41 -0600

    cleaning code

--- src/components/Modales/ModalContacto.tsx
+++ src/components/Modales/ModalContacto.tsx
@@ -8,7 +8,7 @@ import { ModalProps } from "../types/modal.interface";
 
 export const ModalContacto = ({ open, setOpen }: ModalProps) => {
   return (
-    <Dialog open={!open} onClose={setOpen} className="relative z-10">
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"

fd8688f22ea88e08191f9551dc4600bf1b1c0a57
[Jorgejalvarez] <[jorge.alvarez@conafor.gob.mx]>
2024-11-15 11:15:12 -0600

    Ajuste estilo Aviso privacidad

--- src/components/Modales/ModalAvisoPrivacidad.tsx
+++ src/components/Modales/ModalAvisoPrivacidad.tsx
@@ -5,7 +5,7 @@ import { XMarkIcon } from "@heroicons/react/20/solid";
 
 export const ModalAvisoPrivacidad = ({ open, setOpen }: ModalProps) => {
   return (
-    <Dialog open={open} onClose={setOpen} className="relative z-10">
+    <Dialog open={!open} onClose={setOpen} className="relative z-10">
       <DialogBackdrop
         transition
         className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
@@ -14,7 +14,7 @@ export const ModalAvisoPrivacidad = ({ open, setOpen }: ModalProps) => {
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="h-auto w-auto relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            className="h-auto w-auto relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95 p-5 m-5 sm:p-10 sm:m-10"
           >
             <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
               <button

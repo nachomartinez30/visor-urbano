7b2d3a933aea8f545714f82336ab7c5e0b3b010f
[Jorgejalvarez] <[jorge.alvarez@conafor.gob.mx]>
2024-11-26 10:12:27 -0600

    Youtube image for links and mobile header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -122,10 +122,10 @@ export const Header = () => {
               <div className="flex h-16 items-center justify-start">
                 <div className="inset-y-0 items-center visible sm:visible xl:hidden">
                   {/* Mobile menu button*/}
-                  <Link href="/">
-                    <Image alt="logo" className="h-14 w-auto" src={logo} />
-                  </Link>
                   <DisclosureButton className="group inline-flex items-center justify-center rounded-md p-2 text-white-400 hover:bg-verdeFondoIconos hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white navVisor__Control--nav">
+                    <Link href="/" aria-hidden="true" className="block group-data-[open]:hidden navVisor__Control--logo">
+                      <Image alt="logo" className="h-14 w-auto" src={logo} />
+                    </Link>
                     <span className="sr-only">Menú</span>
                     <Bars3Icon
                       aria-hidden="true"
